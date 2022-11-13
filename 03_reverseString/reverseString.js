6301c51d9cc7028e8e602e95188cc9b325e4b4df
ferdwin7 <ferdian.winarta@gmail.com>
2022-11-13 21:27:15 +0800

    Exercise 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const chars = string.split('');
+    let finalindex = chars.length - 1;
+    const revArr = [];
+    for (let i = finalindex; i >= 0; i--) {
+        revArr.push(chars[i]);
+    }
+    let reversed = revArr.join('');
+    return reversed;
 };
 
 // Do not edit below this line
