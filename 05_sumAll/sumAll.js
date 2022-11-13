9848f9d5aad2a1ae15c1918547acb0c2a5d56d87
ferdwin7 <ferdian.winarta@gmail.com>
2022-11-13 23:34:41 +0800

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    if (args.length != 2) {
+        return "ERROR";
+    }
+    else if (args.some(isNaN)) {
+        return "ERROR";
+    }
+    else if (args.some((item) => item < 0)) {
+        return "ERROR";
+    }
+    else if (args.some((item) => !Number.isInteger(item))) {
+        return "ERROR";
+    }
+    else { 
+        let finalSum = 0;
+        if(parseInt(args[0]) < parseInt(args[1])) {
+            startPoint = parseInt(args[0]);
+            endPoint = parseInt(args[1]);
+        }
+        else {
+            startPoint = parseInt(args[1]);
+            endPoint = parseInt(args[0]);
+        }
+        console.log(startPoint, endPoint);
+        for (let i = startPoint; i <= endPoint; i++) {
+            finalSum = finalSum + i;
+        }
+        return finalSum;
+    }
 };
 
 // Do not edit below this line
