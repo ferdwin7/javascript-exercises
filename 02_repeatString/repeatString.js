7e8fb657fdc00a92b89e9d707ebc24083ace3eab
ferdwin7 <ferdian.winarta@gmail.com>
2022-11-13 15:38:01 +0800

    exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, counter) {
+    if(counter >= 0) {
+        let concatenated = '';
+        for (let i = 0; i <= counter - 1; i++) {
+            concatenated = concatenated + string;
 
+        }
+        return concatenated;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
