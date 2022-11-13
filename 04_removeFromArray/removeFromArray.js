2c1c29ef42afdce4ace44f64bd6d1d41782ba791
ferdwin7 <ferdian.winarta@gmail.com>
2022-11-13 22:58:53 +0800

    Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const secondArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            secondArray.push(item);
+        }
+    })
+    return secondArray;
 };
+
+// const array = args[0];
+// console.log(array);
+// const secondArray = [];
+// array.forEach(myFunction(item)); 
+// why does this produce errorrrrrrrrrrrrr, fkkkkkkkk, and why does putting () produce error but no () has no error. This is BULLSHIT!
+
+// function myFunction(item) {
+//     if (!args.includes(item)) {
+//         secondArray.push(item);
+//     }
+// }
+// return secondArray;
 
 // Do not edit below this line
 module.exports = removeFromArray;
