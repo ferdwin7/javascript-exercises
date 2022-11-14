ac317427dff04a225dc11ecd10e71fa52c630aa4
ferdwin7 <ferdian.winarta@gmail.com>
2022-11-14 21:23:03 +0800

    last one

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  let cels = (fahr - 32) * 5/9;
+  cels = parseFloat(cels.toFixed(1));
+  return cels;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  let fahr = 9/5 * cels + 32;
+  fahr = parseFloat(fahr.toFixed(1));
+  return fahr;
 };
 
 // Do not edit below this line
